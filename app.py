8c5b4eafbad30d41ec9a9480fce06ddb05d062b8
Gabriel Gibb <gabrielgibb1@gmail.com>
2023-04-29 14:33:47 -0700

    init

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+@app.route('/test')
+def test():
+    return 'hi'
+
+app.run(host='0.0.0.0')
