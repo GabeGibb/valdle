6eab9d3b163f0565d20d885254be2f8439cf2859
Gabriel Gibb <gabrielgibb1@gmail.com>
2023-04-29 20:37:06 -0700

    oops

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    return render_template('templates/index.html')
 
 @app.route('/test')
 def test():
