2e96fa78d3e5c0e1c79bf1fc4d450a7547b46665
kellyserafico <seraficok@gmail.com>
2023-07-18 16:04:01 -0700

    confetti

--- static/guessAbility.js
+++ static/guessAbility.js
@@ -222,8 +222,50 @@ function isCorrectAbilityName(button, currAbility){
         whatAbilityNameDiv.disabled = true;
         canGuessAbilityName = false;
         $('#finalVictoryMessage').text('yippie!');
+        // confetti();
+
+        var end = Date.now() + (15 * 1000);
+
+        var colors = ['#bb0000', '#ffffff'];
+        
+        (function frame() {
+          confetti({
+            particleCount: 2,
+            angle: 60,
+            spread: 55,
+            origin: { x: 0 },
+            colors: colors
+          });
+          confetti({
+            particleCount: 2,
+            angle: 120,
+            spread: 55,
+            origin: { x: 1 },
+            colors: colors
+          });
+        
+          if (Date.now() < end) {
+            requestAnimationFrame(frame);
+          }
+        }());
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
     else if (canGuessAbilityName){
         button.classList.add("incorrectAbilityName")
     }
 }
+
+
+
