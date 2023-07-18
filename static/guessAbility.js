28e177db8220cae795b2a5696219f8886bbb5773
kellyserafico <seraficok@gmail.com>
2023-07-18 16:50:47 -0700

    include the api next time idot...

--- static/guessAbility.js
+++ static/guessAbility.js
@@ -223,7 +223,35 @@ function isCorrectAbilityName(button, currAbility){
         canGuessAbilityName = false;
         $('#finalVictoryMessage').text('yippie!');
         
-        winConfetti();
+        function winConfetti(){
+
+            var end = Date.now() + (5 * 1000);
+        
+            var colors = ['#bb0000', '#ffffff'];
+            
+            (function frame() {
+              confetti({
+                particleCount: 2,
+                angle: 60,
+                spread: 55,
+                origin: { x: 0 },
+                colors: colors
+              });
+              confetti({
+                particleCount: 2,
+                angle: 120,
+                spread: 55,
+                origin: { x: 1 },
+                colors: colors
+              });
+            
+              if (Date.now() < end) {
+                requestAnimationFrame(frame);
+              }
+            }());
+        
+        
+        }
     }
     else if (canGuessAbilityName){
         button.classList.add("incorrectAbilityName")
