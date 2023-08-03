ab05472774cf4935a6ff41cde77ac5d4bf774be5
DED010 <evrahnos1966@gmail.com>
2023-08-03 01:32:24 -0700

    WIP for audio hint for guessquote (just doesnt get audio link)

--- static/guessQuote.js
+++ static/guessQuote.js
@@ -4,6 +4,7 @@ let quoteUrl = window.location.href + '/quoteOfDay';
 
 let audioFile;
 let quote;
+var audioHintTries = 5;
 
 // jQuery.ajaxSetup({async:false});
 $.get(quoteUrl, function(data, status){ //url defined in current webpage js file
@@ -13,8 +14,26 @@ $.get(quoteUrl, function(data, status){ //url defined in current webpage js file
     quote = data['quote'];
     audioFile = data['audioFile'];
 
-    $('#guessText').text(quote)
+    $('#guessText').text(quote);
+    $('#audioHintText').text("Tries until audio clue: " + audioHintTries);
 });
+
+
+function playAudio(audioPlayer) {
+    audioPlayer.play();
+}
+
+function addAudioElement(audioSrc) {
+    const audioContainer = document.getElementById('audioContainer');
+    const audioPlayer = document.createElement('audio');
+    audioPlayer.src = audioSrc;
+    audioContainer.appendChild(audioPlayer);
+
+    playAudioButton.addEventListener('click', function() {
+        playAudio(audioPlayer);
+    });
+    audioContainer.appendChild(playAudioButton);
+}
 
 
 function curGamemode(){
@@ -29,5 +48,13 @@ function displayPartTwo(){
 }
 
 function modeWrongActions(){
-
+    audioHintTries--;
+    if (audioHintTries == 0) {
+        $('#audioHintText').text("Audio clue:");
+        $('.hintDiv').append("<div id='audioContainer' type='audio/mpeg'></div> <button class='playAudioButton' id='playAudioButton'><img id='imgAudioButton' src=static/images/audioPlay.png></button>");
+        addAudioElement(audioFile);
+    }
+    else {
+        $('#audioHintText').text("Tries until audio clue: " + audioHintTries);
+    }
 }
