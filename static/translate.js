98483fe5d53f8171507514e68c3fc71deaa24eec
DED010 <evrahnos1966@gmail.com>
2023-08-18 19:54:25 -0700

    language wip, testing some stuff

--- static/translate.js
+++ static/translate.js
@@ -1,3 +1,19 @@
+$(document).ready(function() {
+    // Allows toggle with globe button, between visible and hidden dropdown language menu
+    const button = document.getElementById('changeLanguageButton');
+    const dropdown = document.getElementById('languageDropdown');
+
+    button.addEventListener('click', function () {
+        dropdown.style.visibility = dropdown.style.visibility == "visible" ? "hidden" : "visible";
+        dropdown.style.position = dropdown.style.position == "relative" ? "absolute" : "relative";
+    });
+
+    $(".goog-te-gadget-icon").attr("id", "testing1");
+});
+
 function googleTranslateElementInit() {
-    new google.translate.TranslateElement({pageLanguage: 'en'}, 'languageSelector');
+    new google.translate.TranslateElement({
+        includedLanguages: 'en,es',  // Languages to display in the dropdown
+        layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+    }, 'languageDropdown');
 }
