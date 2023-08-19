7f96370a051cf8df29c8e8d4fa30df434380d790
DED010 <evrahnos1966@gmail.com>
2023-08-18 21:03:38 -0700

    about page now updates with selected language

--- static/translate.js
+++ static/translate.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     // Allows toggle with globe button, between visible and hidden dropdown language menu
     const button = document.getElementById('changeLanguageButton');
-    const dropdown = document.getElementById('languageDropdown');
+    const dropdown = document.getElementById('google_translate_element');
 
     button.addEventListener('click', function () {
         dropdown.style.visibility = dropdown.style.visibility == "visible" ? "hidden" : "visible";
@@ -12,8 +12,21 @@ $(document).ready(function() {
 });
 
 function googleTranslateElementInit() {
+    const storedLanguage = localStorage.getItem('selectedLanguage') || 'en';
     new google.translate.TranslateElement({
+        defaultLanguage: storedLanguage,
         includedLanguages: 'en,es',  // Languages to display in the dropdown
         layout: google.translate.TranslateElement.InlineLayout.SIMPLE
-    }, 'languageDropdown');
+    }, 'google_translate_element');
 }
+
+function translatePageContent() {
+    const selectedLanguage = localStorage.getItem('selectedLanguage') || 'en';
+    new google.translate.TranslateElement({
+        defaultLanguage: selectedLanguage,
+        pageLanguage: 'auto',
+        includedLanguages: 'en,es',
+        layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+    }, 'google_translate_element');
+}
+
