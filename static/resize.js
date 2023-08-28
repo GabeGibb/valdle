d616fb1455433748344b0b23d3b833485775f25d
GabeGibb <97437160+GabeGibb@users.noreply.github.com>
2023-08-27 21:31:09 -0700

    response design test

--- static/resize.js
+++ static/resize.js
@@ -1,1 +1,19 @@
-$(window).width(300)
+
+// Change the viewport value based on screen.width
+var viewport_set = function() {
+    if ( window.innerWidth > 500 ){
+        // document.body.style.zoom = 100 + "%";
+    }
+    else{
+        let newZoom = 100 * window.innerWidth / (500) 
+        document.body.style.zoom = newZoom + "%";
+    }
+}
+
+// Set the correct viewport value on page load
+viewport_set();
+
+// Set the correct viewport after device orientation change or resize
+window.onresize = function() { 
+	viewport_set(); 
+}
