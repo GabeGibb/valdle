aa65ab869a63faeb3067f7be7d5666cc561391ce
DED010 <evrahnos1966@gmail.com>
2023-08-27 19:24:18 -0700

    reenabled language button (must change formatting)

--- static/translate.js
+++ static/translate.js
@@ -2,7 +2,7 @@ function googleTranslateElementInit() {
     const storedLanguage = localStorage.getItem('selectedLanguage') || 'en';
     new google.translate.TranslateElement({
         defaultLanguage: storedLanguage,
-        includedLanguages: 'en',  // Languages to display in the dropdown es DISABLED
+        includedLanguages: 'en,es',  // Languages to display in the dropdown es DISABLED
         layout: google.translate.TranslateElement.InlineLayout.SIMPLE
     }, 'google_translate_element');
 }
