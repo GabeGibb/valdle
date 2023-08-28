a5c619038dceded97e385b3edfef97477660e8ba
GabeGibb <97437160+GabeGibb@users.noreply.github.com>
2023-08-27 21:44:52 -0700

    resize test

--- static/resize.js
+++ static/resize.js
@@ -1,11 +1,21 @@
 
 // Change the viewport value based on screen.width
+let threshhold = 650;
+
+if (document.title == 'Guess The Map'){
+    threshhold = 775;
+}
+
+if (document.title == 'Valdle'){
+    threshhold = 550;
+}
+
 var viewport_set = function() {
-    if ( window.innerWidth > 500 ){
+    if ( window.innerWidth > threshhold ){
         document.body.style.zoom = 100 + "%";
     }
     else{
-        let newZoom = 100 * window.innerWidth / (500) 
+        let newZoom = 100 * window.innerWidth / (threshhold) 
         document.body.style.zoom = newZoom + "%";
     }
 }
