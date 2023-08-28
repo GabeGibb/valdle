906770231ceefeafe75f354d4598133cbb4c9a3b
GabeGibb <97437160+GabeGibb@users.noreply.github.com>
2023-08-27 21:32:36 -0700

    fixed smnal bughigsfd

--- static/resize.js
+++ static/resize.js
@@ -2,7 +2,7 @@
 // Change the viewport value based on screen.width
 var viewport_set = function() {
     if ( window.innerWidth > 500 ){
-        // document.body.style.zoom = 100 + "%";
+        document.body.style.zoom = 100 + "%";
     }
     else{
         let newZoom = 100 * window.innerWidth / (500) 
