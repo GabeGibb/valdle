2ae96d12ff448e62b6c2fd1e6dd3bd155dedc9b0
GabeGibb <97437160+GabeGibb@users.noreply.github.com>
2023-09-23 20:38:45 -0700

    small audio and visual changes

--- static/audio.js
+++ static/audio.js
@@ -1,22 +1,32 @@
 
 let audioLength = 0;
 let totalAudioLength;
+let playCounter = 0;
 
 function makeAudioElement(){
     $('.hintDiv').append("<div id='audioContainer' style='display:none' type='audio/mpeg'><button class='playAudioButton' id='playAudioButton'><img id='imgAudioButton' src=static/images/audioPlay.png></button></div>");
 }
 
 function playAudio(audioPlayer) {
-    if (audioPlayer.currentTime != 0){
+    // if (audioPlayer.currentTime != 0){
+    //     audioPlayer.pause();
+    //     audioPlayer.currentTime = 0;
+    //     return
+    // }
+
+    if (audioPlayer.currentTime * 1000 > audioLength){
         audioPlayer.pause();
         audioPlayer.currentTime = 0;
-        return
     }
+    playCounter += 1;
+    let curCounter = playCounter;
 
     audioPlayer.play();
     setTimeout(()=> {
-        audioPlayer.pause();
-        audioPlayer.currentTime = 0;
+        if(curCounter == playCounter){
+            audioPlayer.pause();
+            audioPlayer.currentTime = 0;
+        }
     }, audioLength);
 }
 
