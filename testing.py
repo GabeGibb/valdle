e9ce8213b513b6e1669887488b1ae7f6dde082d1
GabeGibb <97437160+GabeGibb@users.noreply.github.com>
2023-10-22 19:23:22 -0700

    testing update

--- testing.py
+++ testing.py
@@ -1,5 +1,19 @@
 from app import app
+import json
+from json import dumps
+from generateDailyAnswers import generateDailyAnswers
 #hi :3
+
+@app.route('/doThisIfYouWantToDebug')
+def debug():
+    f = open('dailyAnswers.json')
+    past = json.load(f)
+
+    json_object = dumps(generateDailyAnswers(past), indent=4)
+
+    with open("dailyAnswers.json", "w") as outfile:
+        outfile.write(json_object)
+    return json_object
 
 app.run(host='0.0.0.0', debug=True)
 # app.run(host='0.0.0.0')
