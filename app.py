89f130d8dd6b4b4d4658728f42470bc4fdf56cfa
GabeGibb <97437160+GabeGibb@users.noreply.github.com>
2024-06-23 00:51:42 -0700

    not setting cache control

--- app.py
+++ app.py
@@ -29,8 +29,8 @@ def add_header(response):
         response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
         response.headers['Pragma'] = 'no-cache'
         response.headers['Expires'] = '0'
-    else:
-        response.headers['Cache-Control'] = 'public, max-age=3600'
+    # else:
+    #     response.headers['Cache-Control'] = 'public, max-age=3600'
     return response
 
 
