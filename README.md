c2a1a5f73c2fada9abe0a26f65a204f0f71f76c9
GabeGibb <97437160+GabeGibb@users.noreply.github.com>
2024-09-29 17:42:27 -0700

    updated abyss

--- README.md
+++ README.md
@@ -12,8 +12,8 @@
 
 
 # Updating game info
-- Run the downloadApi script (downloads agents / weapons)
+- Run the downloadApi script (downloads agents / weapons / maps)
 - Update guessAgent.js manually (gender and year at the top)
 - Add the quote to the agent manually in the quotes_en.json file (IN CORRECT INDEX POSITION)
-- Map is done via downloading and checking. Uncomment js code in guessMap to debug. Add to html.
+- Map is done via downloading and checking. Uncomment js code in guessMap to debug. Add to html. Feed images to compress_image script and compress with tiny png.
 - Test anomalys with generateDailyAnswers
--- compress_images.py
+++ compress_images.py
@@ -2,10 +2,10 @@ import os
 from PIL import Image
 
 # Loop through all directories and files
-for root, dirs, files in os.walk('./static/'):
+for root, dirs, files in os.walk("./Valorant Maps/Abyss"):
     for file in files:
         # Check if the file is an image
-        if file.lower().endswith(('.png', '.jpg')):
+        if file.lower().endswith((".png", ".jpg")):
             # Get the full path of the image file
             image_path = os.path.join(root, file)
 
@@ -13,8 +13,8 @@ for root, dirs, files in os.walk('./static/'):
             image = Image.open(image_path)
 
             # Convert the image to lossless webp format
-            webp_path = image_path.replace(os.path.splitext(file)[1], '.webp')
-            image.save(webp_path, 'webp', lossless=True)
+            webp_path = image_path.replace(os.path.splitext(file)[1], ".webp")
+            image.save(webp_path, "webp", lossless=True)
 
             # Optional: Delete the original image file
             os.remove(image_path)
