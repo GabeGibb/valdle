94f5bc25237e1844154a3e5684c9481b1fe6fb77
GabeGibb <97437160+GabeGibb@users.noreply.github.com>
2024-11-24 20:54:03 -0800

    fixed

--- updateDailyAnswers.py
+++ updateDailyAnswers.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 masterKey = os.getenv('MASTER')
-
+# TODO: Move genederate and update to api and requirements and call from root directory (dont cd into api)
 
 
 url = 'https://api.jsonbin.io/v3/b/64db06b59d312622a3915ec6'
@@ -14,8 +14,9 @@ headers = {
 'X-Master-Key': masterKey
 }
 
+print("getting current daily answers")
 req = get(url, headers=headers).json()['record']
-
+print("generating new daily answers")
 dailyGameAnswers = generateDailyAnswers(req)
 
 
@@ -25,11 +26,11 @@ headers = {
   'Content-Type': 'application/json',
   'X-Master-Key': masterKey
 }
-
+print("updating daily answers")
 putReq = put(url, json=dailyGameAnswers, headers=headers)
 
 
 
-serverUrl = 'https://valdle.gg/ifYouAreAUserPleaseDontDoThisEndpoint'
+serverUrl = 'https://valdle-nmdu.onrender.com/ifYouAreAUserPleaseDontDoThisEndpoint'
 changeAnswers = get(serverUrl)
 print(changeAnswers.content)
