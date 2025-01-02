181d61de2fecb6b1207e32cef49e9588b6fd2258
GabeGibb <97437160+GabeGibb@users.noreply.github.com>
2025-01-01 16:27:35 -0800

    added home screen

--- medal.py
+++ medal.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "https://developers.medal.tv/v1/latest?categoryId=fW3AZxHf_c&limit=10&text=valdle"
+url = "https://developers.medal.tv/v1/latest?categoryId=fW3AZxHf_c&limit=10&text=%23valdle"
 headers = {
   "Authorization": "pub_1T2PUQmnPFfoxKMmUUfvn7iBWJLosear"
 }
